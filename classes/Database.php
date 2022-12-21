aac398f82b88d3061b1d06445c1e90b78297367c
Zarha Buske <zarha.buske@medieinstitutet.se>
2022-12-21 12:03:53 +0100

    Database setup

---
+++ classes/Database.php
@@ -0,0 +1,19 @@
+<?php
+class Database
+{
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "root";
+    private $db = "examensarbete_db";
+
+
+    protected $conn;
+
+    public function __construct()
+    {
+        $this->conn = mysqli_connect($this->host, $this->user, $this->pass, $this->db);
+        if (!$this->conn) {
+            die("Error connecting to db.");
+        }
+    }
+}

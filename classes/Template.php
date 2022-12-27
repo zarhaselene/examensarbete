a66b536f81fbe9c6ca228530c7d7880fe13d11cf
Zarha Buske <zarha.buske@medieinstitutet.se>
2022-12-27 11:41:24 +0100

    Template setup

--- classes/Template.php
+++ classes/Template.php
@@ -6,7 +6,6 @@ class Template
     {
 
 ?>
-
         <!DOCTYPE html>
         <html lang="en">
 
@@ -14,14 +13,20 @@ class Template
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
             <title> <?= $title ?> </title>
             <link rel="stylesheet" href="/exa/assets/style.css">
+            <!-- Fonts -->
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&family=Varela+Round&display=swap" rel="stylesheet">
+
+            <!-- Font Awesome -->
+            <script src="https://kit.fontawesome.com/57a104828d.js" crossorigin="anonymous"></script>
 
         </head>
 
         <body>
-            Test
+
         </body>
 
         </html>

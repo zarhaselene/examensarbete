38bb7ef7c08f473896eaeced228ad90d707f235e
Zarha Buske <zarha.buske@medieinstitutet.se>
2023-01-02 17:58:22 +0100

    Slider script updated

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,22 @@
-//  Initialize Swiper
-var swiper = new Swiper(".mySwiper", {
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-});
+// Sliders
+const swiperContainers = document.querySelectorAll(".slider");
+
+for (const swiperContainer of swiperContainers) {
+	const swiper = swiperContainer.querySelector(".swiper");
+
+	new Swiper(swiper, {
+		loop: true,
+		pagination: {
+			el: ".swiper-pagination",
+			dynamicBullets: true,
+		},
+		// Navigation arrows
+		navigation: {
+			nextEl: swiperContainer.querySelector(".swiper-button-next"),
+			prevEl: swiperContainer.querySelector(".swiper-button-prev"),
+		},
+	});
+}
 
 // Scroll
 $(window).scroll(function () {

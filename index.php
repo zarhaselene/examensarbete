f79ec413fc5895c81a5ccd7797e5694758e92692
Zarha Buske <zarha.buske@medieinstitutet.se>
2023-01-02 14:51:38 +0100

    Hero slides added

--- index.php
+++ index.php
@@ -32,10 +32,32 @@ Template::header('Matley sound');
 
             </div>
             <div class="swiper-slide">
+                <div class="hero-container">
+                    <div class="hero-text">
+                        <div class="headings">
+                            <h3 class="color-white">Live Smart </h3>
+                            <h1 class="color-light-pink">Hear </h1>
+                            <h2 class="color-white">Smart</h2>
+                        </div>
+                        <p class="color-white">You bring the music, we bring the quality. Make music a pleasant experience. </p>
+                        <a class="btn slider-btn" href="/exa/index.php">Shop Now</a>
+                    </div>
+                </div>
                 <img class="hide-tablet" src="/exa/assets/img/hero_slider_2.jpg" alt="">
                 <img class="show-tablet" src="/exa/assets/img/hero_slider_2_phone.jpg" alt="">
             </div>
             <div class="swiper-slide">
+                <div class="hero-container">
+                    <div class="hero-text">
+                        <div class="headings">
+                            <h3 class="color-white">Less Noice </h3>
+                            <h1 class="color-light-pink">More </h1>
+                            <h2 class="color-white">Sound</h2>
+                        </div>
+                        <p class="color-white">Headphones that make you forget everything around. Make music come to life. </p>
+                        <a class="btn slider-btn" href="/exa/index.php">Shop Now</a>
+                    </div>
+                </div>
                 <img class="hide-tablet" src="/exa/assets/img/hero_slider_3.jpg" alt="">
                 <img class="show-tablet" src="/exa/assets/img/hero_slider_3_phone.jpg" alt="">
             </div>

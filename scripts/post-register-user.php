3c5fb3771d5cee420c75ab3851162c5f998d1059
Zarha Buske <zarha.buske@medieinstitutet.se>
2023-01-03 16:41:55 +0100

    Username length limit

--- scripts/post-register-user.php
+++ scripts/post-register-user.php
@@ -9,7 +9,7 @@ if (
     isset($_POST['username']) &&
     isset($_POST['password']) &&
     isset($_POST['confirm-password']) &&
-    strlen($_POST['username']) > 0 &&
+    strlen($_POST['username']) <= 10 && //String limit: 10
     strlen($_POST['password']) > 0 &&
     $_POST['password'] === $_POST['confirm-password']
 ) {

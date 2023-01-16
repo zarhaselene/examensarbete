d235d47226884e036e9433c2f53f666ba3c73004
Zarha Buske <zarha.buske@medieinstitutet.se>
2023-01-16 15:02:02 +0100

    cart styling

--- pages/cart.php
+++ pages/cart.php
@@ -22,51 +22,57 @@ Template::header('Cart'); ?>
 <section class="shopping-cart position-relative">
 
     <?php if (!$products) : ?>
-        <a class="go-back-btn" href="/exa/index.php"> <- Continue shopping</a>
+        <a class="go-back-btn" href="/exa/index.php"> <i class='bx bx-arrow-back'></i> Continue shopping</a>
 
-                <!-- No products  -->
-            <?php elseif ($_SESSION["cart"]) : ?>
+        <!-- No products  -->
+    <?php elseif ($_SESSION["cart"]) : ?>
 
-                <form action="/exa/scripts/post-place-order.php" method="post">
+        <form action="/exa/scripts/post-place-order.php" method="post">
 
-                    <div class="row">
-                        <div class="col-16">
-                            <a class="go-back-btn" href="/exa/index.php"> <- Continue shopping</a>
-                                    <div class="heading display-flex align-items-center">
-                                        <h2>Shopping cart</h2>
-                                        <span class="p-h-5 color-pink"><b><?= $cart_count ?> items</b> </span>
+            <div class="row">
+                <div class="col-16 display-flex direction-column justify-end ">
+                    <div class="wrapper display-flex direction-column justify-between">
+                        <div class="go-back-btn display-flex align-items-center">
+
+                            <a class="color-white href=" /exa/index.php"><i class='bx bx-arrow-back m-h-1'></i><span>Continue shopping</span>
+                            </a>
+                        </div>
+                        <div class="heading display-flex align-items-center">
+                            <h2>Shopping cart</h2>
+                            <span class="p-h-5 color-pink"><b><?= $cart_count ?> items</b> </span>
+                        </div>
+                        <div class="cart-items-container">
+
+                            <?php foreach ($products as $product) : ?>
+                                <div class="cart-item m-b-4 display-flex align-items-center">
+                                    <input type="hidden" name="id" value="<?= $product->id ?>">
+                                    <div class="image p-r-2">
+                                        <img class="products-cart-img" src="<?= $product->img_url ?>" alt="product-img">
                                     </div>
-                                    <?php foreach ($products as $product) : ?>
-                                        <div class="cart-item m-v-2 display-flex align-items-center">
-                                            <input type="hidden" name="id" value="<?= $product->id ?>">
-                                            <div class="image p-r-2">
-                                                <img class="products-cart-img" src="<?= $product->img_url ?>" alt="product-img">
-                                            </div>
-                                            <div class="item-info">
-                                                <h3><?= $product->title ?></h3>
-                                                <p>$<?= $product->price ?></p>
-                                            </div>
+                                    <div class="item-info">
+                                        <h3><?= $product->title ?></h3>
+                                        <p>$<?= $product->price ?></p>
+                                    </div>
+                                </div>
+                                <div class="hr"></div>
 
-                                        </div>
-
-                                    <?php endforeach ?>
-
-
+                            <?php endforeach ?>
                         </div>
-                        <div class="col-8">
-                            <p>PAYMENT</p>
-                            <span class="cart-total">
-                                <h3>Total price: $<?= $total ?></h3>
-                            </span> <?php if ($is_logged_in) : ?>
-                                <input class="cart-button" type="submit" value="Place order">
-                            <?php endif; ?>
-
-                            <?php if (!$is_logged_in) : ?>
-                                <p>You need to be logged in to place an order</p>
-                            <?php endif; ?>
-                        </div>
-                </form>
-
+                    </div>
                 </div>
-            <?php endif ?>
+                <div class="col-8">
+                    <?php if ($is_logged_in) : ?>
+                        <span class="cart-total">
+                            <h3>Total: $<?= $total ?></h3>
+                        </span>
+                        <button class="cart-button btn full-btn " type="submit">Place order</button>
+                    <?php endif; ?>
+
+                    <?php if (!$is_logged_in) : ?>
+                        <p>You need to be logged in to place an order</p>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </form>
+    <?php endif ?>
 </section>

df794800652542fe9700654b8de8f24f3bf243db
Zarha Buske <zarha.buske@medieinstitutet.se>
2023-01-16 17:12:13 +0100

    updated

--- scripts/post-login.php
+++ scripts/post-login.php
@@ -1,26 +1,5 @@
 <?php
 require_once __DIR__ . "/../classes/UsersDatabase.php";
-// session_start();
-
-// $success = false;
-
-// if (isset($_POST['username']) && isset($_POST['password'])) {
-//     $users_db = new UsersDatabase();
-//     $user = $users_db->get_one_by_username($_POST['username']);
-//     if ($user && $user->test_password($_POST['password'])) {
-//         session_start();
-//         $_SESSION['user'] = $user;
-
-//         header('Location: /exa/index.php');
-//     } else {
-//         header('Location: /exa/index.php?error=invalid_credentials');
-//         die();
-//     }
-// } else {
-//     header('Location: /exa/index.php?error=invalid_credentials');
-//     die();
-// }
-
 
 session_start();
 
@@ -53,3 +32,5 @@ if ($success) {
     header('Location: /exa/index.php?error=invalid_credentials');
     die();
 }
+//error=invalid_credentials = Invalid username or password!
+
--- scripts/post-register-user.php
+++ scripts/post-register-user.php
@@ -5,41 +5,13 @@ require_once __DIR__ . "/../classes/UsersDatabase.php";
 
 $success = false;
 
-// if (
-//     isset($_POST['username']) &&
-//     isset($_POST['password']) &&
-//     isset($_POST['confirm-password']) &&
-//     strlen($_POST['username']) <= 10 && //String limit: 10
-//     strlen($_POST['password']) > 0 &&
-//     $_POST['password'] === $_POST['confirm-password']
-// ) {
-//     $users_db = new UsersDatabase();
-
-//     $user = new User($_POST['username'], 'customer');
-//     $user->hash_password($_POST['password']);
-
-//     $existing_user = $users_db->get_one_by_username($_POST['username']);
-//     if ($existing_user) {
-//         die('Username is taken');
-//     } else {
-//         $success = $users_db->create($user);
-//     }
-// } else {
-//     header('Location: /exa/index.php?error=invalid_register');
-//     die();
-// }
-
-// // felmeddelande 
-// if ($success) {
-//     header("Location: /exa/index.php?register=success");
-// } else {
-//     header('Location: /exa/index.php?error=invalid_register');
-//     die();
-// }
-
 if (
-    isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["confirm-password"])
-    && $_POST["password"] === $_POST["confirm-password"]
+    isset($_POST['username']) &&
+    isset($_POST['password']) &&
+    isset($_POST['confirm-password']) &&
+    strlen($_POST['username']) <= 10 && //String limit: 10
+    strlen($_POST['password']) > 0 &&
+    $_POST['password'] === $_POST['confirm-password']
 ) {
     $username = $_POST["username"];
     $password = $_POST["password"];
@@ -52,12 +24,13 @@ if (
     $existing_user = $users_db->get_one_by_username($username);
 
     if ($existing_user) {
-        die('Username is taken');
+        header('Location: /exa/index.php?error=invalid_register_username');
+        die();
     } else {
         $success = $users_db->create($user);
     }
 } else {
-    echo "ERROR: Invalid input.";
+    header('Location: /exa/index.php?error=invalid_register');
     die();
 }
 
@@ -67,3 +40,6 @@ if ($success) {
     header('Location: /exa/index.php?error=invalid_register');
     die();
 }
+//error=invalid_register = Error saving user, try again.
+//error=invalid_register_username = Username taken, try again.
+//register=success register=success = User created, please log in!

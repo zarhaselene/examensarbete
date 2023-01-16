ba7b95414ed1be35089bca7c8fde2b628465ab45
Zarha Buske <zarha.buske@medieinstitutet.se>
2023-01-16 17:39:15 +0100

    google logout merged with logout script

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . "/google-config.php";
-
-//Reset OAuth access token
-$google_client->revokeToken();
-//Destroy entire session data.
-session_destroy();
-//redirect page to index.php
-header('Location: /exa');
--- scripts/post-logout.php
+++ scripts/post-logout.php
@@ -1,5 +1,11 @@
 <?php
+require_once __DIR__ . "/../google-config.php";
+
 session_start();
+//Reset OAuth access token
+$google_client->revokeToken();
 unset($_SESSION['user']);
+//Destroy entire session data.
 session_destroy();
+//redirect page to index.php
 header('Location: /exa');

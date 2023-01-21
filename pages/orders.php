1f85af3858f3b52bf5f052e130ac6d4ad6e6ce04
Zarha Buske <zarha.buske@medieinstitutet.se>
2023-01-21 04:08:11 +0100

    My orders page started

--- pages/orders.php
+++ pages/orders.php
@@ -6,47 +6,111 @@ require_once __DIR__ . "/../classes/UsersDatabase.php";
 $logged_in_user = $_SESSION["user"];
 
 $orders_db = new OrdersDatabase();
-// $users_db = new UsersDatabase();
-
-// $users = $users_db->get_all();
 
 $orders = $orders_db->get_order_by_user_id($logged_in_user->id);
 
-Template::header("Orders page");
+Template::header("My account - Orders");
 ?>
+<section class="my-account">
+    <div class="account-nav display-flex align-items-center">
+        <div class="heading m-z display-flex align-items-center">
+            <h2>Your Orders</h2>
+            <p class=" p-h-1">(<?= count($orders) ?>)</p>
+            <a class="go-back-btn" href="/exa/index.php"> <i class='bx bx-arrow-back'></i> Continue shopping</a>
+        </div>
+    </div>
+    <div class="orders-container">
+        <div class="row">
+            <div class="col-7">
 
-<h2>My orders</h2>
+                <div class="order-list">
+                    <?php foreach ($orders as $order) :
+                        $products = $orders_db->get_products_by_order($order->id);
+                        $total_price = 0;
+                        $product_count = 0;
+                        foreach ($products as $product) :
+                            $total_price += $product->price;
+                            $product_count++;
+                        endforeach;
+                    ?>
+                        <div class="order-item">
+                            <div>
+                                <div class="display-flex align-items-center justify-between">
+                                    <p class="order-id">
+                                        <b>#4812<?= $order->id ?></b>
+                                    </p>
+                                    <span class="order 
+                                        <?php
+                                        if ($order->status === "Pending") {
+                                            echo "pending";
+                                        } elseif ($order->status === "Sent") {
+                                            echo "sent";
+                                        } elseif ($order->status === "Hold") {
+                                            echo "hold";
+                                        } ?>
+                                        ">
+                                        <?= $order->status ?>
+                                    </span>
+                                </div>
+                                <div class="display-flex justify-between">
+                                    <div class="display-flex direction-column">
+                                        <span><b>$<?= $total_price ?>.00</b></span>
+                                        <span><?= $product_count ?> Items</span>
+                                    </div>
+                                    <small> <?= $order->order_date ?>
+                                    </small>
+                                </div>
 
-<?php foreach ($orders as $order) :
-    $products = $orders_db->get_products_by_order($order->id);
-    $total_price = 0;
-?>
-    <p>
-        <b>Order (# <?= $order->id ?>)</b>
-        [<?= $order->status ?>]
-    </p>
+                            </div>
+                        </div>
 
-    <?php foreach ($products as $product) :
-        $total_price += $product->price;
-    ?>
-        <div class="profile-show-all">
-            <p class="link">
-                <?php echo $product->title ?>
-                <?php echo " - $" . $product->price ?>
-            </p>
+
+                    <?php endforeach; ?>
+                </div>
+
+            </div>
+            <div class="col-17">
+                <div class="order-overview">
+
+                </div>
+            </div>
         </div>
 
-    <?php endforeach; ?>
-
-    <div class="total-price">
-        <p>
-            <?php echo "Total price: $" . $total_price ?>
-        </p>
     </div>
+</section>
+<!-- 
+<?php foreach ($orders as $order) :
+    $products = $orders_db->get_products_by_order($order->id);
 
-    <br>
-<?php endforeach;
+    $total_price = 0;
+?>
+                    <p>
+                        <b>Order (# <?= $order->id ?>)</b>
+                        [<?= $order->status ?>]
+                    </p>
+
+                    <?php foreach ($products as $product) :
+                        $total_price += $product->price;
+                    ?>
+                        <div class="profile-show-all">
+                            <p class="link">
+                                <?php echo $product->title ?>
+                                <?php echo " - $" . $product->price ?>
+                            </p>
+                        </div>
+
+                    <?php endforeach; ?>
+
+                    <div class="total-price">
+                        <p>
+                            <?php echo "Total price: $" . $total_price ?>
+                        </p>
+                    </div>
+
+                    <br>
+                <?php endforeach; ?>
+ -->
 
 
-
+<?
 Template::footer();

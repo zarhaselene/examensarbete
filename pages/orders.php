d914592aae2ae3197d7a403cd93c53012263aeee
Zarha Buske <zarha.buske@medieinstitutet.se>
2023-01-22 22:12:35 +0100

    Order page changed

--- pages/orders.php
+++ pages/orders.php
@@ -49,26 +49,23 @@ Template::header("My account - Orders");
         </div>
     </div>
     <div class="orders-container">
-        <div class="row">
-            <div class="col-7">
-
-                <ul class="order-list">
-                    <h2 class="p-l-5 p-b-2">Orders History</h2>
-                    <?php foreach ($orders as $order) :
-                        $products = $orders_db->get_products_by_order($order->id);
-                        $total_price = 0;
-                        $product_count = 0;
-                        foreach ($products as $product) :
-                            $total_price += $product->price;
-                            $product_count++;
-                        endforeach;
-                    ?>
-                        <li class="order-item">
-                            <div class="display-flex align-items-center justify-between">
-                                <p class="order-id">
-                                    <b>#4812<?= $order->id ?></b>
-                                </p>
-                                <span class=" order 
+        <ul class="order-list">
+            <h2 class="p-l-5 p-b-2">Orders History</h2>
+            <?php foreach ($orders as $order) :
+                $products = $orders_db->get_products_by_order($order->id);
+                $total_price = 0;
+                $product_count = 0;
+                foreach ($products as $product) :
+                    $total_price += $product->price;
+                    $product_count++;
+                endforeach;
+            ?>
+                <li class="order-item">
+                    <div class="display-flex align-items-center justify-between">
+                        <p class="order-id">
+                            <b>#4812<?= $order->id ?></b>
+                        </p>
+                        <span class=" order 
                                         <?php
                                         if ($order->status === "Pending") {
                                             echo  "pending";
@@ -78,67 +75,21 @@ Template::header("My account - Orders");
                                             echo "canceled";
                                         } ?>
                                         ">
-                                    <?= $order->status ?>
-                                </span>
-                            </div>
-                            <div class="display-flex justify-between">
-                                <div class="display-flex direction-column">
-                                    <span><b>$<?= $total_price ?>.00</b></span>
-                                    <span><?= $product_count ?> Items</span>
-                                </div>
-                                <small> <?= $order->order_date ?>
-                                </small>
-                            </div>
-
-                        </li>
-
-
-                    <?php endforeach; ?>
-                </ul>
-
-            </div>
-            <div class="col-17">
-                <div class="order-overview">
-
-                </div>
-            </div>
-        </div>
-
+                            <?= $order->status ?>
+                        </span>
+                    </div>
+                    <div class="display-flex justify-between">
+                        <div class="display-flex direction-column">
+                            <span><b>$<?= $total_price ?>.00</b></span>
+                            <span><?= $product_count ?> Items</span>
+                        </div>
+                        <small> <?= $order->order_date ?>
+                        </small>
+                    </div>
+                </li>
+            <?php endforeach; ?>
+        </ul>
     </div>
 </section>
-<!-- 
-<?php foreach ($orders as $order) :
-    $products = $orders_db->get_products_by_order($order->id);
-
-    $total_price = 0;
-?>
-                    <p>
-                        <b>Order (# <?= $order->id ?>)</b>
-                        [<?= $order->status ?>]
-                    </p>
-
-                    <?php foreach ($products as $product) :
-                        $total_price += $product->price;
-                    ?>
-                        <div class="profile-show-all">
-                            <p class="link">
-                                <?php echo $product->title ?>
-                                <?php echo " - $" . $product->price ?>
-                            </p>
-                        </div>
-
-                    <?php endforeach; ?>
-
-                    <div class="total-price">
-                        <p>
-                            <?php echo "Total price: $" . $total_price ?>
-                        </p>
-                    </div>
-
-                    <br>
-                <?php endforeach; ?>
- -->
-
-
 <?
 Template::footer();
